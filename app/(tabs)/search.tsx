5873e68562ab7a4e29b583fcb1bc24c3fb71e8ce
sebastianj7 <sebastianjacobs2000@gmail.com>
2024-09-16 14:55:28 +0200

    feat: :sparkles: first api call
    first api call to get Discover movies
    
    none

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaView } from 'react-native'
 
 const Search = () => {
   return (
